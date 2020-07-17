0ea69ffb9bac750cc23c359500ef58ab28007290
Claire <claire.moore@proquest.com>
2020-07-17 14:36:05 +0100

    Refactoring notes

--- world_profile/controller.py
+++ world_profile/controller.py
@@ -1,5 +1,8 @@
 import world_profile.world_profile as wp
 
+# You would normally have one controller per model, so this should be renamed/stored to make it clear that this is the controller for world profile.
+# You would also have one view implementation per view you're exposing.
+
 def process_decode_request(code):
     return wp.WorldProfile.from_code(code)
 
